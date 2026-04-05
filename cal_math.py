5b4fa792c5e033a6d3fbebd22aebbef728d8d56c
sammaiah98 <sammaiahpenaka98@gmail.com>
2026-04-05 09:27:21 +0000

    added multi

--- cal_math.py
+++ cal_math.py
@@ -5,3 +5,9 @@ def add_num(a, b):
 print("Operation completed")
 print("test CD is working or not")
 print("test cd")
+
+def multi_num(a, b):
+  """This is Multiplication Functions"""
+  return a*b
+
+print("Operation completed")
